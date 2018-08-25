b99a48b7e2b6e0111d16d1d23adbee8e736b1cf2
satanas <wil.alejandro@gmail.com>
2018-08-25 00:54:49 -0700

    Adding delete button for instructions

--- js/scenes/programming.js
+++ js/scenes/programming.js
@@ -3,22 +3,28 @@ HEX_BASE = 32512;
 class ProgrammingScene extends Scene {
   constructor() {
     super();
-    this.instructions = [ACTIONS.FW, ACTIONS.FW, ACTIONS.TR, ACTIONS.FW, ACTIONS.FW, ACTIONS.TL, ACTIONS.RP];
+    //this.instructions = [ACTIONS.FW, ACTIONS.FW, ACTIONS.TR, ACTIONS.FW, ACTIONS.FW, ACTIONS.TL, ACTIONS.RP];
+    this.instructions = [ACTIONS.FW];
     this.instPanel = new InstructionsPanel(240, 40);
     this.instPanel.enabled = false;
     this.addBtn = new AddButton(20, 0, () => { this.instPanel.enabled = true; });
+
+    this.btnGroup = new Group();
+    this.btnGroup.add(this.addBtn);
+
     $.listen(this, 'addInstruction');
   }
 
   addInstruction(ev) {
     this.instructions.push(ev.detail.inst);
     this.instPanel.enabled = false;
+    this.btnGroup.add(new DelButton(210, 16 + (20 * this.instructions.length)));
   }
 
   update() {
     let yCoord = 50 + (20 * this.instructions.length);
     this.addBtn.setPos({y: yCoord});
-    this.addBtn.update();
+    this.btnGroup.update();
     this.instPanel.update();
   }
 
@@ -37,7 +43,7 @@ class ProgrammingScene extends Scene {
       $.txt.render('0x' + (HEX_BASE + i).toString(16) + ': ' + op, 20, 40 + (20 * i), '#fff', 7);
     }
 
-    $.cam.render(this.addBtn);
+    $.cam.render(this.btnGroup);
     $.cam.render(this.instPanel);
   }
 }
@@ -47,6 +53,7 @@ class InstructionsPanel extends Sprite {
     super(x, y, 200, 400);
     this.btnGroup = new Group();
     let i = 0, a;
+    // TODO: Use map?
     for(let a in ACTIONS) {
       this.btnGroup.add(new InstructionButton(x + 10, y + 10 + (i * 30), ACTIONS[a]));
       i++;
@@ -90,6 +97,29 @@ class InstructionButton extends UIButton {
   }
 }
 
+class DelButton extends UIButton {
+  constructor(x, y, cb) {
+    super(x, y, 16, 16);
+    this.cb = cb;
+  }
+
+  onClick() {
+    this.cb();
+  }
+
+  update() {
+    this.checkClick();
+  }
+
+  render(rect) {
+    $.ctx.save();
+    $.ctx.fillStyle = 'red';
+    $.ctx.fillRect(rect.x, rect.y, this.w, this.h);
+    $.txt.render('X', rect.x + 4, rect.y + 4, '#fff', 6);
+    $.ctx.restore();
+  }
+}
+
 class AddButton extends UIButton {
   constructor(x, y, cb) {
     super(x, y, 210, 25);
@@ -106,7 +136,7 @@ class AddButton extends UIButton {
 
   render(rect) {
     $.ctx.save();
-    $.ctx.fillStyle = 'red';
+    $.ctx.fillStyle = 'purple';
     $.ctx.fillRect(rect.x, rect.y, this.w, this.h);
     $.txt.render('Add instruction', rect.x + 40, rect.y + 8, '#fff', 6);
     $.ctx.restore();
