236eb2a879174f102d32f52b12c3ac11060f6f14
satanas <wil.alejandro@gmail.com>
2018-09-02 19:16:31 -0700

    Rendering cables with cable sprite

--- js/scenes/connect.js
+++ js/scenes/connect.js
@@ -3,9 +3,22 @@ class ConnectCables extends Scene {
     super();
 
     this.num = 3;
+    this.padding = 200;
+    this.slot = ($.vw - this.padding) / this.num;
     this.colors = shuffle(['red', 'green', 'blue', 'orange', 'black']).slice(0, this.num);
     this.upper = shuffle(this.colors);
     this.lower = shuffle(this.colors);
+
+    let i;
+    this.upperCables = new Group();
+    for (i = 0; i < this.upper.length; i++) {
+      this.upperCables.add(new Cable(i, this.upper[i], this.padding, this.slot, true));
+    }
+
+    this.lowerCables = new Group();
+    for (i = 0; i < this.lower.length; i++) {
+      this.upperCables.add(new Cable(i, this.lower[i], this.padding, this.slot, false));
+    }
 
     this.maxTimer = 5000;
     this.timer = this.maxTimer;
@@ -21,36 +34,20 @@ class ConnectCables extends Scene {
 
   render() {
     $.cam.clear('#ccc');
-    $.ctx.save();
-
-    let i, x,
-        h = 180,
-        w = 32,
-        padding = 200,
-        slot = ($.vw - padding) / this.num;
 
     // Render upper cables
-    for (i = 0; i < this.upper.length; i++) {
-      x = (padding / 2) + (slot * i) + (slot / 2) - (w / 2);
-
-      $.ctx.fillStyle = this.upper[i];
-      $.ctx.fillRect(x, 0, w, h);
-    }
+    $.cam.render(this.upperCables);
 
     // Render lower cables
-    for (i = 0; i < this.lower.length; i++) {
-      x = (padding / 2) + (slot * i) + (slot / 2) - (w / 2);
+    $.cam.render(this.lowerCables);
 
-      $.ctx.fillStyle = this.lower[i];
-      $.ctx.fillRect(x, $.vh - h, w, h);
-    }
-
+    $.ctx.save();
     // Render progress bar bg
     $.ctx.fillStyle = 'rgba(55,255,0,0.2)';
     $.ctx.fillRect(0, 0, $.vw, 20);
 
     // Render progress bar fg
-    w = this.timer * $.vw / 5000;
+    let w = this.timer * $.vw / 5000;
     $.ctx.fillStyle = 'rgba(55,255,0,0.5)';
     $.ctx.fillRect(0, 0, w, 20);
 
@@ -58,7 +55,35 @@ class ConnectCables extends Scene {
   }
 }
 
-//slot 206 x 184
-//slot 206 x 287
-//slot 206 x 390
-//slot 206 x 493
+class Sensor extends Sprite {
+  constructor(x, y, color) {
+    super(x, y, 16, 16);
+  }
+
+  render(rect) {
+    $.ctx.save();
+
+    $.ctx.fillStyle = 'green';
+    $.ctx.fillRect(rect.x, rect.y, w, h);
+    $.crx.restore();
+  }
+
+}
+
+class Cable extends Sprite {
+  constructor(i, color, padding, slot, upper) {
+    let h = 180,
+        w = 32,
+        x = (padding / 2) + (slot * i) + (slot / 2) - (w / 2),
+        y = (upper) ? 0 : $.vh - h;
+    super(x, y, w, h);
+    this.color = color;
+  }
+
+  render(rect) {
+    $.ctx.save();
+    $.ctx.fillStyle = this.color;
+    $.ctx.fillRect(rect.x, rect.y, this.w, this.h);
+    $.ctx.restore()
+  }
+}
