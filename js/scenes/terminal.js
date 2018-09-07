62f38901151797bef54991cbf82c4bb45588fb46
satanas <wil.alejandro@gmail.com>
2018-09-07 00:39:42 -0700

    Rendering fully functional terminal

--- js/scenes/terminal.js
+++ js/scenes/terminal.js
@@ -1,44 +1,76 @@
 class TerminalScene extends Scene {
   constructor(output) {
+    output = [
+      ['Fusion power generator', 1],
+      ['Life support system', 0],
+      ['Nuclear engines', 0],
+      ['Nuclear engines', 0],
+      ['Nuclear engines', 0],
+      ['Nuclear engines', 0],
+      ['Nuclear engines', 0],
+    ];
     super();
-    this.output = this.booting().concat(output || []);
+    this.output = output || [];
+    this.booting = [
+      ['AEROS ROM Operating System', 0],
+      ['Copyright 2412 by Hyper-Tec', 0],
+      ['', 0],
+      ['Exec Version 23.93', 0],
+      ['', 0],
+      ['Initializing...', 0],
+      ['Loading neural network', 100],
+      ['Loading FxD-6 module', 0],
+      ['Loading quantic matrix', 0],
+      ['Loading hyperspacial sensors', 0],
+      ['', 0],
+      ['Analyzing ship status...', 400],
+      ['', 0],
+    ];
     this.padding = 5;
     this.topMargin = 30;
     this.prompt = new Prompt(10, this.topMargin + this.padding);
-    this.maxCharTime = 25;
+    this.maxCharTime = 15;
     this.charTime = 0;
     this.lineHeight = 18;
     this.charWidth = 10;
     this.lineIndex = 0;
     this.charIndex = 0;
     this.font = new TextRenderer("monospace", "#0f0", 16);
-    this.done = false;
-  }
-
-  booting() {
-    return [
-      ['AEROS ROM Operating System', 0],
-      ['Copyright 2412 by Hyper-Tec', 0],
-      ['', 0],
-      ['Exec Version 23.93', 600],
-      ['', 0],
-      ['Loading FxD-6 module...', 0],
-      ['', 0]
-    ];
+    this.rFont = new TextRenderer("monospace", "#f00", 16);
+    this.booted = false;
+    this.analyzed = false;
   }
 
   update() {
     let i, text;
-    this.charTime -= this.deltaTime;
-    if (this.charTime <= 0 && !this.done) {
-      this.charTime = this.maxCharTime;
-      this.charIndex += 1;
-      if (this.charIndex > this.output[this.lineIndex][0].length) {
-        this.charIndex = 0
+    if (!this.booted) {
+      this.charTime -= this.deltaTime;
+
+      if (this.charTime <= 0) {
+        this.charIndex += 1;
+        this.charTime = this.getCharDelay();
+
+        if (this.charIndex > this.booting[this.lineIndex][0].length) {
+          this.charIndex = 0
+          this.lineIndex += 1;
+          if (this.lineIndex > this.booting.length - 1) {
+            this.booted = true;
+            this.lineIndex = 0;
+            // Delay during analyzing phase
+            this.charTime = 1000;
+          }
+        }
+      }
+    }
+
+    if (this.booted && !this.analyzed) {
+      this.charTime -= this.deltaTime;
+
+      if (this.charTime <= 0) {
         this.lineIndex += 1;
+        this.charTime = 200;
         if (this.lineIndex > this.output.length - 1) {
-          //this.lineIndex = this.output.length - 1;
-          this.done = true;
+          this.analyzed = true;
         }
       }
     }
@@ -46,16 +78,23 @@ class TerminalScene extends Scene {
     this.prompt.update(this.deltaTime);
   }
 
+  getCharDelay() {
+    if (this.charIndex === this.booting[this.lineIndex][0].length) {
+      let delay = this.booting[this.lineIndex][1];
+      return (delay) ? delay : this.maxCharTime;
+    }
+    return this.maxCharTime;
+  }
+
   getPartialText(index) {
     let i, text = '';
     for (i = 0; i < this.charIndex; i++) {
-      text += this.output[index][0].charAt(i);
+      text += this.booting[index][0].charAt(i);
     }
     return text;
   }
 
   render() {
-    //$.cam.clear('#382400');
     $.cam.clear('#00405c');
 
     $.ctx.save();
@@ -63,16 +102,42 @@ class TerminalScene extends Scene {
     $.ctx.fillRect(this.padding, this.topMargin, $.vw - (this.padding * 2), $.vh - (this.topMargin + this.padding));
     $.ctx.restore();
 
-    let i, lineY;
-    for (i = 0; i < this.output.length; i++) {
+    let i, lineY, lineX, txt;
+    for (i = 0; i < this.booting.length; i++) {
       lineY = this.topMargin + this.padding + (this.lineHeight * (i + 1));
-      if (i < this.lineIndex || this.done) {
-        this.font.render($.ctx, this.output[i][0], this.padding * 2, lineY);
+      if (i < this.lineIndex || this.booted) {
+        this.font.render($.ctx, this.booting[i][0], this.padding * 2, lineY);
       } else if (i === this.lineIndex) {
         this.prompt.x = this.padding + (this.charIndex * this.charWidth) + 5;
         this.prompt.y = lineY - 14;
         this.font.render($.ctx, this.getPartialText(i), this.padding * 2, lineY);
       }
+    }
+
+    if (this.booted) {
+      lineX = 300;
+      for (i = 0; i < this.output.length; i++) {
+        if (i < this.lineIndex || this.analyzed) {
+          txt = this.output[i][0];
+          lineY = this.topMargin + this.padding + (this.lineHeight * (this.booting.length + i + 1));
+          this.prompt.x = this.padding * 2; //lineX + 90;
+          this.prompt.y = lineY + this.lineHeight - 14;
+          this.font.render($.ctx, txt, this.padding * 2, lineY);
+
+          if (this.output[i][1]) {
+            this.font.render($.ctx, '[ONLINE]', lineX, lineY);
+          } else {
+            this.rFont.render($.ctx, '[OFFLINE]', lineX, lineY);
+          }
+        }
+      }
+    }
+
+    if (this.analyzed) {
+      lineY = this.topMargin + this.padding + (this.lineHeight * (this.booting.length + this.output.length + 4));
+      this.prompt.x = 245;
+      this.prompt.y = lineY - 14;
+      this.font.render($.ctx, 'Touch screen to continue', this.padding * 2, lineY);
     }
 
     $.cam.render(this.prompt);
